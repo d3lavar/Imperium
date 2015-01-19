6698f60815ab443ac4fa44ca2eb1afa5ceb23735
d3lavar <d3lavar@gmail.com>
2015-01-19 23:07:01 +0400

    Area changes

--- src/main/scala/imperium/engine/core/position/Area.scala
+++ src/main/scala/imperium/engine/core/position/Area.scala
@@ -11,9 +11,13 @@ object Area {
 
 class Area(val point: Point, val width: Int, val height: Int) {
 
+  if (point == null) throw new IllegalArgumentException("Point can only be a non-nil instance of Point")
+  if (width < 0) throw new IllegalArgumentException("Width can only be a positive integer.")
+  if (height < 0) throw new IllegalArgumentException("Height can only be a positive integer.")
+
   def includes(point: Point): Boolean = {
     point.getX >= this.point.getX && point.getX <= this.point.getX + width &&
-    point.getY >= this.point.getY && point.getY <= this.point.getY + height
+      point.getY >= this.point.getY && point.getY <= this.point.getY + height
   }
 
 }
