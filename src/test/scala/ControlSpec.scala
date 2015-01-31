ac4a2e86bee5f157285851d99f3d49c677214409
d3lavar <d3lavar@gmail.com>
2015-01-31 12:16:51 +0400

    ControlSpec changes

--- src/test/scala/ControlSpec.scala
+++ src/test/scala/ControlSpec.scala
@@ -9,8 +9,7 @@ class ControlSpec extends FreeSpec with Matchers {
 
   lazy val area = new Area(AreaPositioner(1, 1).topPoint, 1, 1)
 
-  "it can be active or not" in {
-    val control = new TestControl
+  "it can be active or not" in new Control {
     assert(!control.isActive)
     control.onHoverActivated()
     assert(control.isActive)
@@ -18,20 +17,22 @@ class ControlSpec extends FreeSpec with Matchers {
     assert(!control.isActive)
   }
 
-  "it can have area" in {
-    val control = new TestControl
+  "it can have area" in new Control {
     control.area should be(area)
   }
 
-  "should produce NotImplementedError when render called" in {
+  "should produce NotImplementedError when render called" in new Control {
     intercept[NotImplementedError] {
-      val control = new TestControl
       control.render()
     }
   }
 
   class TestControl extends AbstractControl(area, (_) => Unit) {
     override def render(): Unit = ???
+  }
+
+  class Control {
+    val control = new TestControl
   }
 
 }
